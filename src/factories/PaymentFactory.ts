9a0e5079ae247eec5c9716d39ad312d6d579d99e
Derek Provance <derek.provance@formstack.com>
2023-08-20 11:59:41 -0500

    Modifed to have generic venmo email for processing

--- src/factories/PaymentFactory.ts
+++ src/factories/PaymentFactory.ts
@@ -8,18 +8,16 @@ export class PaymentFactory {
     public static createPayment(email: string, subject: string): IPayment | undefined {
         let payment;
 
-        switch (email) {
-            case config.paymentProcessor.venmo.email:
-                logger.debug('Creating payment object for Venmo Email');
-                try {
-                    payment = new VenmoPayment(subject);
-                } catch (ex) {
-                    logger.debug(ex);
-                }
-                break;
-            default:
-                logger.debug('NOTICE: Not a handled email. Nothing to process.');
-                throw new InvalidPayment(`Unhandled Email Detected: ${email}`);
+        if (email.includes(config.paymentProcessor.venmo.email)) {
+            logger.debug('Creating payment object for Venmo Email');
+            try {
+                payment = new VenmoPayment(subject);
+            } catch (ex) {
+                logger.debug(ex);
+            }
+        } else {
+            logger.debug('NOTICE: Not a handled email. Nothing to process.');
+            throw new InvalidPayment(`Unhandled Email Detected: ${email}`);
         }
 
         return payment;
